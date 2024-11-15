d07c8e6913ae4e9118c22006a1295c763f398c1f
unknown <yassin.erraouch@gmail.com>
2024-11-15 11:26:59 +0100

    added python loops quiz 'serie_boucle.pdf'

--- Python/Structures_alternatives/exo25(temps restant à un anniversaire).py
+++ Python/Structures_alternatives/exo25(temps restant à un anniversaire).py
@@ -1,35 +1,3 @@
-# from datetime import datetime, date
-
-# # Demandez à l'utilisateur d'entrer sa date de naissance (jour et mois seulement)
-# jour_naissance = int(input("Entrez le jour de votre naissance (1-31) : "))
-# mois_naissance = int(input("Entrez le mois de votre naissance (1-12) : "))
-
-# # Récupérez la date actuelle
-# date_actuelle = date.today()
-
-# # Calculez la date de l'anniversaire de cette année
-# anniversaire_cette_annee = date(date_actuelle.year, mois_naissance, jour_naissance)
-
-# # Calculez la date de l'anniversaire de l'année prochaine
-# anniversaire_annee_prochaine = date(date_actuelle.year + 1, mois_naissance, jour_naissance)
-
-# # Vérifiez si l'anniversaire est déjà passé pour cette année
-# if anniversaire_cette_annee < date_actuelle:
-#     # Si oui, utilisez la date de l'anniversaire de l'année prochaine
-#     anniversaire_a_venir = anniversaire_annee_prochaine
-# else:
-#     # Sinon, utilisez la date de l'anniversaire de cette année
-#     anniversaire_a_venir = anniversaire_cette_annee
-
-# # Calculez le nombre de jours jusqu'à l'anniversaire à venir
-# jours_jusqu_anniversaire = (anniversaire_a_venir - date_actuelle).days
-
-# # Gérez le cas où l'anniversaire tombe aujourd'hui
-# if jours_jusqu_anniversaire == 0:
-#     print("Joyeux anniversaire !")
-# else:
-#     print(f"Il vous reste {jours_jusqu_anniversaire} jours jusqu'à votre prochain anniversaire.")
-
 import datetime
 
 #ask user for a day and a month
@@ -41,3 +9,26 @@ while True:
     except:
         print("entrez des valeurs valides")
 
+#get current date
+currentDate = datetime.date.today()
+
+#calculate the birthday date this year
+bDayThisYear = datetime.date(currentDate.year, birthMonth, birthDay)
+
+#calculate the birthday date next year
+bDayNextYear = datetime.date(currentDate.year + 1, birthMonth, birthDay)
+
+#check if the birthday is already passed this year
+if bDayThisYear < currentDate:
+    bDay = bDayNextYear
+else:
+    bDay = bDayThisYear
+
+#calculate the number of days until the birthday
+daysUntilBirthday = (bDay - currentDate).days
+
+#display the result
+if daysUntilBirthday == 0:
+    print("Joyeux anniversaire !")
+else:
+    print(f"Il vous reste {daysUntilBirthday} jours jusqu à votre prochain anniversaire.")
