3f062ed99dd7f9a07c1e06b9f446120122e4d97b
Your Name <2000090800482@ofppt-edu.ma>
2024-11-28 12:54:31 +0100

    added new tps and their answers

--- blank.py
+++ blank.py
@@ -1,1 +1,11 @@
-print(list(range(3, 30, 7)))
+try: 
+    choice = int(input("Enter a number: "))
+except:
+    print('invalid value')
+    
+    
+def use_choice (choice):
+    choice += 10
+    return choice
+
+print(use_choice(choice))
