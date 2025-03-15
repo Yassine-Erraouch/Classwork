0203cb75bd60ab611bba1982c5968bd1ab3e2f0d
Yassine-Erraouch <yassine.erraouch@gmail.com>
2025-03-15 11:56:36 +0000

    made some minor adjustments to countries.js and countries.html

--- JavaScript/EFMs/EFM FesMeknes/Exo2/countries.js
+++ JavaScript/EFMs/EFM FesMeknes/Exo2/countries.js
@@ -1,28 +1,27 @@
-let countries = [
-    "Afrique du Sud",
-    "Allemagne",
-    "Australie",
-    "Brésil",
-    "Canada",
-    "Chine",
-    "Corée du Sud",
-    "Egypte",
-    "Etats-Unis",
-    "France",
-    "Inde",
-    "Italie",
-    "Japon",
-    "Maroc",
-    "Mexique",
-    "Nouvelle-Zélande",
-    "Russie",
-    "Thaïlande",
-    "Turquie",
-    "Vietnam"
-];
 
-document.querySelector('#countryOptions').innerHTML = countries.map((item)=> 
+// List of coutnries by continent
+const countriesByContinent = {
+    Afrique: ["Nigeria", "Éthiopie", "Égypte", "Afrique du Sud", "Kenya", "Algérie", "Maroc", "Ghana", "Angola", "Ouganda"],
+    Asie: ["Chine", "Inde", "Indonésie", "Pakistan", "Bangladesh", "Japon", "Philippines", "Vietnam", "Turquie", "Iran"],
+    Europe: ["Allemagne", "France", "Royaume-Uni", "Italie", "Espagne", "Ukraine", "Pologne", "Roumanie", "Pays-Bas", "Belgique"],
+    Amériques: ["États-Unis", "Canada", "Mexique", "Guatemala", "Cuba", "Haïti", "République Dominicaine", "Honduras", "El Salvador", "Nicaragua", "Brésil", "Argentine", "Colombie", "Chili", "Pérou", "Venezuela", "Équateur", "Bolivie", "Paraguay", "Uruguay"],
+    Océanie: ["Australie", "Papouasie-Nouvelle-Guinée", "Nouvelle-Zélande", "Fidji", "Îles Salomon", "Vanuatu", "Samoa", "Kiribati", "Tonga", "Micronésie"],
+    Antarctique: ["Aucun"]
+};
+
+const continents = Object.keys(countriesByContinent);
+document.querySelector('#contSelect').innerHTML += continents.map((item)=> 
     `<option value='${item}'>${item}</option>`).join('');
+
+document.querySelector('#contSelect').addEventListener('change', (e) => {
+    let continent = e.target.value;
+    let countries = countriesByContinent[continent];
+    document.querySelector('#countryOptions').innerHTML = countries.map((item)=> 
+    `<option value='${item}'>${item}</option>`).join('');
+});
+
+// document.querySelector('#contSelect').innerHTML = countries.map((item)=> 
+//     `<option value='${item}'>${item}</option>`).join('');
 
 
 
@@ -73,11 +72,17 @@ let remove = (i) => {
 }
 
 let add = () => {
-    let cont =  document.querySelector('#contInput');
+    let cont =  document.querySelector('#contSelect');
     let country = document.querySelector('#countryOptions');
 
-    if (cont!= "" && country.length>0) {
-        
+    if (cont.value != "" && country.value.length>0) {
+        alert('Veuillez selectionner un continent et un ou plusieurs pays');
+        return;
+    }
+
+    let pays = [];
+    for (let i = 0; i < country.value.length; i++) {
+        pays.push(country.value[i]);
     }
 }
 
